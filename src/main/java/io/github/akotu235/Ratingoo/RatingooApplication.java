6b8c7a65beaf9447acf8230c1802e14f21b6f922
akotu235 <akotu@protonmail.com>
2022-12-28 13:56:08 +0100

    initial commit

---
+++ src/main/java/io/github/akotu235/Ratingoo/RatingooApplication.java
@@ -0,0 +1,13 @@
+package io.github.akotu235.Ratingoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/akotu235/Ratingoo/RatingooApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.akotu235.Ratingoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
